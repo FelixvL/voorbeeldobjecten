0214748e9cd30a7c906a09f5bb88cb577224cf92
Felixvl <digitaleservice@gmail.com>
2018-03-27 11:33:03 +0200

    frontend werkt

--- functions.js
+++ functions.js
@@ -1,3 +1,21 @@
 function nieuwtournooi(){
-    alert("werkt in nieuw tournooi");
+    var tournooi = {};
+    tournooi.naam = document.getElementById("naamtournooi").value;
+    tournooi.stad = document.getElementById("stadtournooi").value;
+    tournooi.tijdstip = document.getElementById("tijdstiptournooi").value;
+    tournooi.prijs = document.getElementById("prijstournooi").value;
+    var raymon = new XMLHttpRequest();
+    raymon.onreadystatechange = function (){
+        if(this.readyState === 4 && this.status === 200){
+            console.log(this.responseText);
+        }
+    }
+    var data = JSON.stringify(tournooi);
+    raymon.open("POST", "nieuwtournooi.php", true);
+    raymon.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    raymon.send("tournooi="+data);
+    
+}
+function nieuwvechter(){
+    alert("werkt in nieuw vechter");
 }
--- index.php
+++ index.php
@@ -17,5 +17,12 @@ include "functions.php";
         Tijdstip Tournooi<input type="text" id="tijdstiptournooi"><br>
         Prijs Tournooi<input type="text" id="prijstournooi"><br>
         <input type="button" value="voeg toe" onclick="nieuwtournooi();">
+        <hr>
+        <hr>
+        Naam Vechter<input type="text" id="naamvechter"><br>
+        Gewicht Vechter<input type="text" id="gewichtvechter"><br>
+        Specialisatie Vechter<input type="text" id="specialisatievechter"><br>
+        SpecialTrick Vechter<input type="text" id="specialtrickvechter"><br>
+        <input type="button" value="voeg toe" onclick="nieuwvechter();">
     </body>
 </html>
